56c716ec87f061feedcb938ed9c499586fbde1c8
ninpnin <vainoyrjanainen@icloud.com>
2022-01-20 16:20:28 +0100

    Delete code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from probabilistic_word_embeddings.models.base_models import SGNS
+from probabilistic_word_embeddings.models.base_models import sgns_likelihood, generate_cbow_batch, generate_sgns_batch
 import numpy as np
 from probabilistic_word_embeddings.embeddings import Embedding
 import tensorflow as tf
@@ -8,12 +8,6 @@ vocabulary = set(text)
 
 vocab_size = len(vocabulary)
 dim = 25
-
-model = SGNS(vocab_size=vocab_size, dim=dim)
-
-print("SGNS")
-print(model)
-
 theta = Embedding(vocabulary=vocabulary, dimensionality=dim)
 
 i = tf.constant(text[1:3])
@@ -23,8 +17,19 @@ x = 1
 
 batch = i,j,x
 print(theta[i])
-lp = model.loss(batch, theta)
+lp = sgns_likelihood(batch, theta)
 
 print(lp)
 
 
+batch = generate_sgns_batch([tf.constant(text)], D=100, ws=5, ns=5, batch=2, start_ix=0, dataset_ix=0)
+
+print(batch)
+
+i, j = batch
+print(i.shape, j.shape)
+
+batch = i,j,x
+lp = sgns_likelihood(batch, theta)
+
+print(lp)
--- probabilistic_word_embeddings/models/base_models.py
+++ probabilistic_word_embeddings/models/base_models.py
@@ -4,40 +4,17 @@ tfd = tfp.distributions
 import probabilistic_word_embeddings.distributions as custom_dist
 
 # Bernoulli Skip-Gram with Negative Samples
-class SGNS:
-    """
-    Bernoulli SGNS model with an arbitrary prior distribution of the embedding \\( \\theta = [\\rho, \\alpha] \\)
-
-    Args:
-        theta: Joint prior distribution of the word and the context vectors
-        ws: Size of the context window
-        ns: Number of negative samples
-        batch_size: Number of data points per training iteration
-    """
-    def __init__(self,vocab_size=None, dim=25):
-        # Define loss function
-        
-        # Copy function attributes to the object
-        self.V, self.D = vocab_size, dim
-        
-        self.theta_split = 0, vocab_size # where to split theta into rho and alpha
-
-    def get_batch(self, data, start_ix, dataset_ix, ws=2, ns=5):
-        i,j = _generate_sgns_batch(data, vocab_size, D=dim, ws=ws, ns=ns,
-                        batch=batch_size, start_ix=start_ix, dataset_ix=dataset_ix)
-        return i, j
-
-    def loss(self, batch, theta):
-        i, j, x = batch[0], batch[1], batch[2]
-        logits = tf.tensordot(theta[i], theta[j], axes=(-1,-1))
-        ps = tf.math.sigmoid(logits)
-        log_ps = tf.math.log(ps)
-        return tf.reduce_sum(log_ps) + theta.log_prob()
+def sgns_likelihood(batch, theta):
+    i, j, x = batch[0], batch[1], batch[2]
+    logits = tf.tensordot(theta[i], theta[j], axes=(-1,-1))
+    ps = tf.math.sigmoid(logits)
+    log_ps = tf.math.log(ps)
+    return tf.reduce_sum(log_ps) + theta.log_prob()
     
 # Generate a random i,j batch of the data.
 #@tf.function
-def _generate_sgns_batch(data, V, D=100, ws=5, ns=5, batch=150000, start_ix=0, dataset_ix=0):
-    i,j = _generate_cbow_batch(data, V, D=D, ws=ws, ns=ns, batch=batch, start_ix=start_ix, dataset_ix=dataset_ix)
+def generate_sgns_batch(data, D=100, ws=5, ns=5, batch=150000, start_ix=0, dataset_ix=0):
+    i,j = generate_cbow_batch(data, D=D, ws=ws, ns=ns, batch=batch, start_ix=start_ix, dataset_ix=dataset_ix)
     
     i = tf.transpose(tf.tile([i], [ws * 2, 1]))
     
@@ -52,7 +29,7 @@ def x_batch_sgns(minibatch, ws, ns):
 
 # Generate a random i,j batch of the data.
 #@tf.function
-def _generate_cbow_batch(datasets, V, D=100, ws=5, ns=5, batch=150000, start_ix=0, dataset_ix=0):
+def generate_cbow_batch(datasets, D=100, ws=5, ns=5, batch=150000, start_ix=0, dataset_ix=0):
     # the dog saw the cat
     # data = [0, 1, 2, ..., 0, 3]
     data = datasets[dataset_ix]
