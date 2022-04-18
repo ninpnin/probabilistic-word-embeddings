80c4668831634120dec91089cd9ebb1fa81ce2cd
ninpnin <vainoyrjanainen@icloud.com>
2022-04-18 09:35:04 +0200

    test: adjust tests to changed api structure

--- tests/map.py
+++ tests/map.py
@@ -1,9 +1,9 @@
 import unittest
 
+from probabilistic_word_embeddings.embeddings import Embedding
 from probabilistic_word_embeddings.preprocessing import preprocess_standard
 from probabilistic_word_embeddings.estimation import map_estimate
-from probabilistic_word_embeddings.embeddings import Embedding
-from probabilistic_word_embeddings.models import CBOW
+from probabilistic_word_embeddings.evaluation import get_eval_file, embedding_similarities, evaluate_word_similarity
 import tensorflow as tf
 import numpy as np
 
@@ -11,24 +11,20 @@ class Test(unittest.TestCase):
 
     # Test MAP estimation with random data
     def test_map(self):
-        V = 17
-        D = 25
-        data_len = 10000
+        text = open("tests/data/0.txt").read().lower().split()
+        text, vocabulary = preprocess_standard(text)
+
+        vocab_size = len(vocabulary)
         batch_size = 250
-
-        prior = Embedding(V * 2, dim=D)
-        model = CBOW(prior, batch_size=batch_size)
-        model.data_size = data_len
-
-        data = np.random.randint(V, size=data_len)
-        data = [data]
-
-        theta = map_estimate(model, data, epochs=1)
+        dim = 25
+        e = Embedding(vocabulary=vocabulary, dimensionality=dim)
+        e = map_estimate(e, text, evaluate=False, epochs=1)
+        theta = e.theta.numpy()
 
         self.assertEqual(type(theta), np.ndarray)
 
         theta_shape = theta.shape
-        valid_shape = (V * 2, D)
+        valid_shape = (vocab_size * 2, dim)
         self.assertEqual(theta_shape, valid_shape)
 
 
