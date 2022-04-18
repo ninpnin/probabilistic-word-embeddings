e48c41e0e166bc4eb57bda989358fda341b82ef6
ninpnin <vainoyrjanainen@icloud.com>
2022-04-18 16:52:56 +0200

    test: make sure that saving works

--- tests/embeddings.py
+++ tests/embeddings.py
@@ -1,24 +1,62 @@
 import unittest
 
-import math
+from probabilistic_word_embeddings.embeddings import Embedding, LaplacianEmbedding
+from probabilistic_word_embeddings.preprocessing import preprocess_standard, preprocess_dynamic
+from probabilistic_word_embeddings.estimation import map_estimate
+from probabilistic_word_embeddings.evaluation import get_eval_file, embedding_similarities, evaluate_word_similarity
 import tensorflow as tf
-import probabilistic_word_embeddings
+import numpy as np
+from pathlib import Path
+import random
+import networkx as nx
 
 class EmbeddingTest(unittest.TestCase):
 
-    # SGNS log probability. Batch size == 2
-    def test_normal_embedding(self):
-        vocab_size, dim = 7, 11
+    def test_embedding_saving_1(self):
+        """
+        Test MAP estimation with example dataset
+        """
+        with open("tests/data/0.txt") as f:
+            text = f.read().lower().split()
+        text, vocabulary = preprocess_standard(text)
 
-        embedding = NormalEmbedding(vocab_size, dim=dim)
-        sample = embedding.sample()
+        vocab_size = len(vocabulary)
+        batch_size = 250
+        dim = 25
+        e = Embedding(vocabulary=vocabulary, dimensionality=dim)
+        saved_model_path = "test.pkl"
+        e.save(saved_model_path)
+        e2 = Embedding(saved_model_path=saved_model_path)
 
-        test_shape = sample.shape
-        valid_shape = (7,11)
-        self.assertEqual(test_shape, valid_shape)
+        theta1 = e.theta.numpy()
+        theta2 = e2.theta.numpy()
+        self.assertAlmostEqual(np.max(np.abs(theta1-theta2)), 0.0)
+        self.assertEqual(e.lambda0, e2.lambda0)
 
-        init = embedding.init()
-        log_prob = embedding.log_prob(init)
+    def test_embedding_saving_2(self):
+        """
+        Test MAP estimation with example dataset
+        """
+        with open("tests/data/0.txt") as f:
+            text = f.read().lower().split()
+        text, vocabulary = preprocess_standard(text)
+
+        vocab_size = len(vocabulary)
+        batch_size = 250
+        dim = 25
+
+        graph = nx.Graph()
+        graph.add_edge("this", "that")
+        e = LaplacianEmbedding(vocabulary=vocabulary, dimensionality=dim, graph=graph)
+        saved_model_path = "test.pkl"
+        e.save(saved_model_path)
+        e2 = LaplacianEmbedding(saved_model_path=saved_model_path)
+
+        theta1 = e.theta.numpy()
+        theta2 = e2.theta.numpy()
+        self.assertAlmostEqual(np.max(np.abs(theta1-theta2)), 0.0)
+        self.assertEqual(e.lambda0, e2.lambda0)
+        self.assertEqual(e.lambda1, e2.lambda1)
 
 
 if __name__ == '__main__':
