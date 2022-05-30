15d7b842743ce1026ce633155af5cd0213e337ca
ninpnin <vainoyrjanainen@icloud.com>
2022-05-30 17:45:56 +0300

    docs: adjust demo script to new functionality

--- train_with_validation.py
+++ train_with_validation.py
@@ -2,6 +2,7 @@ from probabilistic_word_embeddings.embeddings import Embedding
 from probabilistic_word_embeddings.preprocessing import preprocess_standard
 from probabilistic_word_embeddings.estimation import map_estimate
 from probabilistic_word_embeddings.evaluation import get_eval_file, embedding_similarities, evaluate_word_similarity
+from probabilistic_word_embeddings.evaluation import evaluate_on_holdout_set
 from scipy.spatial.distance import cosine as cos_dist
 import tensorflow as tf
 
@@ -18,13 +19,14 @@ valid_size = int(valid_size * len(text))
 
 text_train = text[:train_size]
 text_valid = text[train_size:train_size+valid_size]
+text_test = text[train_size+valid_size:]
 print(f"Train on a text of length {len(text)} with a vocabulary size of {len(vocabulary)}")
 
 dim = 100
 e = Embedding(vocabulary=vocabulary, dimensionality=dim)
 
 # Perform MAP estimation
-e = map_estimate(e, text_train, model="cbow", ws=5, valid_data=text_valid, epochs=15, evaluate=False)
+e = map_estimate(e, text_train, model="cbow", ws=5, valid_data=text_valid, epochs=2, evaluate=False, early_stopping=True)
 
 similarity = evaluate_word_similarity(e)
 print(similarity)
@@ -34,5 +36,10 @@ print("Cosdist 'this', 'this'", cos_dist(e["this"], e["this"]))
 print("Cosdist 'this', 'that'", cos_dist(e["this"], e["that"]))
 print("Cosdist 'this', 'man'", cos_dist(e["this"], e["man"]))
 print("Cosdist 'this', 'motion'", cos_dist(e["this"], e["motion"]))
+
+# Evaluate on test set
+test_ll = evaluate_on_holdout_set(e, text_test, model="cbow", ws=5)
+
+print(f"Test set performance: {test_ll}")
 
 e.save("embeddings.pkl")
