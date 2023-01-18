9d55ec367562f2286ed63365e205f5ccb0e2fc62
ninpnin <vainoyrjanainen@icloud.com>
2023-01-18 16:19:34 +0100

    fix: dynamic embedding example

--- dynamic.py
+++ dynamic.py
@@ -1,32 +1,25 @@
-import networkx as nx
-import polars as pl
-import re
-from probabilistic_word_embeddings.embeddings import Embedding, LaplacianEmbedding
-from probabilistic_word_embeddings.preprocessing import preprocess_standard, preprocess_partitioned
+from probabilistic_word_embeddings.embeddings import LaplacianEmbedding
+from probabilistic_word_embeddings.preprocessing import preprocess_partitioned
 from probabilistic_word_embeddings.estimation import map_estimate
-from probabilistic_word_embeddings.evaluation import get_eval_file, embedding_similarities, evaluate_word_similarity
-from scipy.spatial.distance import cosine as cos_dist
-import tensorflow as tf
+import networkx as nx
+import pandas as pd
+import re, itertools
 
 # Aggregate to yearly string objects and their associated years
-yearly = pl.read_csv("yearly.csv")
-yearly = yearly.with_column(pl.col("date") // 10000) # Flatten date to just the year
-texts, years = [], sorted(list(set(yearly["date"])))
+df = pd.read_csv("dynamic.csv")
+texts, years = [], sorted(list(set(df["year"])))
 for year in years:
-	current = yearly.filter(pl.col("date") == year)
-	text = " ".join(current["speech"]).lower()
-	text = re.sub(r'[^ \w+]', '', text)
-	text = text.split()
-	texts.append(text)
+	current = df[df["year"] == year]
+	text = " ".join(current["speech"])
+	text = re.sub(r"\W+ ", '', text).lower()
+	texts.append(text.split())
 
 # Preprocess
 texts, vocabulary = preprocess_partitioned(texts, years)
-text = []
-for t in texts:
-	text = text + t
+text = list(itertools.chain(*texts))
 print(text[:10], text[-10:])
 
-# Create side information graph
+# Create side information graph, dog_2010 ~ dog_2011 etc.
 g = nx.Graph()
 for year0, year1 in zip(years[:-1], years[1:]):
 	for wd in set([wd.split("_")[0] for wd in vocabulary]):
@@ -34,12 +27,12 @@ for year0, year1 in zip(years[:-1], years[1:]):
 		wd1 = f"{wd}_{year1}"
 		if wd0 in vocabulary and wd1 in vocabulary:
 			g.add_edge(wd0, wd1)
-
+print(list(g.edges)[:10])
 
 # Define embedding and perform MAP estimation
 dim = 100
 e = LaplacianEmbedding(vocabulary, dim, g)
-e = map_estimate(e, text, model="cbow", ws=5, epochs=1, evaluate=False)
+e = map_estimate(e, text, model="cbow", ws=5, epochs=5, evaluate=False)
 
 # Save embedding
 e.save("dynamic_embedding.pkl")
