6382a91007720d1d7eb3a23fdc30a15f3823feac
ninpnin <vainoyrjanainen@icloud.com>
2023-04-26 12:48:49 +0200

    test: thoroughly test ELBO

--- tests/estimation.py
+++ tests/estimation.py
@@ -6,6 +6,7 @@ from probabilistic_word_embeddings.estimation import map_estimate, mean_field_vi
 from probabilistic_word_embeddings.evaluation import embedding_similarities, evaluate_word_similarity
 from probabilistic_word_embeddings.evaluation import evaluate_on_holdout_set, bli
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 from pathlib import Path
 import random
@@ -252,18 +253,75 @@ class Test(unittest.TestCase):
         text, vocabulary = preprocess_standard(text)
 
         vocab_size = len(vocabulary)
-        batch_size = 250
-        dim = 25
-        e = Embedding(vocabulary=vocabulary, dimensionality=dim)
+        batch_size = 25
+        ws = 5
+        dim = 2
 
-        ll_before = evaluate_on_holdout_set(e, text, model="cbow", ws=5, batch_size=batch_size)
-        q_mu, q_std_log, elbo_history = mean_field_vi(e, text, model="cbow", evaluate=False, batch_size=batch_size, epochs=10, elbo_history=True)
-        ll_after = evaluate_on_holdout_set(q_mu, text, model="cbow", ws=5, batch_size=batch_size)
+        estimated_elbos = []
+        true_elbos = []
+        error_tolerances = []
 
-        self.assertLess(ll_before, ll_after)
+        for epochs in [5, 15, 25, 50, 50, 50]:
+            e = Embedding(vocabulary=vocabulary, dimensionality=dim)
+            q_mu, q_std_log, elbo_history = mean_field_vi(e, text, model="cbow", evaluate=False, ws=ws, batch_size=batch_size, epochs=epochs, elbo_history=True)
 
-        for elbo_i, elbo_i_plus_1 in zip(elbo_history[:-1], elbo_history[1:]):
-            self.assertLess(elbo_i, elbo_i_plus_1, f"ELBO_i should be smaller than ELBO_i+1 ({elbo_i} vs {elbo_i_plus_1})")
+            q_mean = q_mu.theta.numpy()
+            q_std = tf.exp(q_std_log).numpy()
+
+            rounds = len(vocabulary) // batch_size
+
+            # Calculate true expected value of the posterior
+            posteriors = []
+            for _ in range(rounds):
+                epsilon = tf.random.normal(q_std.shape, dtype=tf.float64)
+                z = q_mean + tf.multiply(q_std, epsilon)
+                e.theta.assign(z)
+                ll = evaluate_on_holdout_set(q_mu, text, model="cbow", ws=ws, batch_size=len(text), reduce_mean=False)
+                posterior = tf.reduce_sum(ll).numpy() + e.log_prob(1, 1)
+                posteriors.append(posterior)
+            expected_posterior = tf.reduce_mean(posteriors)
+
+            # Calculate true entropy
+            q_flat_std = tf.reshape(q_std,[-1])
+            dist = tfp.distributions.MultivariateNormalDiag(loc=tf.zeros(q_flat_std.shape, dtype=q_flat_std.dtype), scale_diag=q_flat_std)
+            entropy = dist.entropy()
+
+            # Calculate true and estimated ELBO
+            estimated_elbo = elbo_history[-1]
+            elbo = expected_posterior + entropy
+            estimated_elbos.append(estimated_elbo)
+            true_elbos.append(elbo)
+
+            # Error due to ELBO improvement during the epoch
+            # error = avg. slope during past 4 epochs
+            optimization_deviation = (elbo_history[-1] - elbo_history[-4]) / 3.0
+            if optimization_deviation < 0.0:
+                optimization_deviation = 0.0
+
+            # Error due to ELBO improvement during the epoch
+            # error = stdev of hat ELBO * 2
+            stochastic_vi_deviation = np.std(elbo_history[-4:]) * 3.0
+            stochastic_vi_deviation_prime = np.std(elbo_history[-4:] - np.array(range(4)) * optimization_deviation ) * 2.0
+            print("Corrected v original", stochastic_vi_deviation_prime, stochastic_vi_deviation)
+            if stochastic_vi_deviation_prime < stochastic_vi_deviation:
+                stochastic_vi_deviation = stochastic_vi_deviation_prime
+                print("Use linear correction for stochastic_vi_deviation")
+
+            # Error due to stochasticity in the calculation of the true posterior
+            # error = standard error of the mean of the posteriors
+            posterior_estimate_deviation = np.std(posteriors) / np.sqrt(rounds) * 3.0
+            print("optimization_deviation", optimization_deviation)
+            print("stochastic_vi_deviation", stochastic_vi_deviation)
+            print("posterior_estimate_deviation", posterior_estimate_deviation)
+            error_tolerances.append(optimization_deviation + stochastic_vi_deviation + posterior_estimate_deviation)
+
+        # Correct for constant offsets
+        constant_offset = tf.reduce_mean(estimated_elbos[-3:]) - tf.reduce_mean(true_elbos[-3:])
+        estimated_elbos = [elbo - constant_offset for elbo in estimated_elbos]
+
+        for elbo, hat_elbo, error_tolerance in zip(true_elbos, estimated_elbos, error_tolerances):
+            print("ELBO vs hat ELBO", elbo, hat_elbo, f"(+-{error_tolerance})")
+            self.assertAlmostEqual(elbo, hat_elbo, delta=error_tolerance)
 
 
 
