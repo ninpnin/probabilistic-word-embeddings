ed4cb305b257bc6920f60e0fb9736380fbfd72d1
ninpnin <vainoyrjanainen@icloud.com>
2024-01-30 17:24:36 +0100

    test: data generator in MAP estimation

--- tests/map.py
+++ tests/map.py
@@ -98,7 +98,7 @@ class Test(unittest.TestCase):
         embeddings = e[words]
         self.assertEqual(embeddings.shape, (K, dim))
 
-    def test_map_with_freqs(self):
+    def test_map_with_generator(self):
         """
         Test MAP estimation with example dataset
         """
@@ -109,8 +109,21 @@ class Test(unittest.TestCase):
         vocab_size = len(vocabulary)
         batch_size = 250
         dim = 25
+        N = len(text)
         e = Embedding(vocabulary=vocabulary, dimensionality=dim)
-        e = map_estimate(e, text, evaluate=False, epochs=1, batch_size=batch_size, vocab_freqs=vocabulary)
+
+        def data_generator():
+            while True:
+                i = np.random.choice(text, batch_size)
+                j = np.random.choice(text, batch_size)
+                x = np.random.binomial(1, 0.25, size=batch_size)
+
+                i = tf.constant(i)
+                j = tf.constant(j)
+                x = tf.constant(x, dtype=tf.float64)
+                yield (i,j,x)
+
+        e = map_estimate(e, data_generator=data_generator(), N=N, evaluate=False, model="sgns", epochs=1, batch_size=batch_size)
         theta = e.theta.numpy()
 
         self.assertEqual(type(theta), np.ndarray)
