f372d8ffa3f2734875a14b715485df9db53c6bb8
ninpnin <vainoyrjanainen@icloud.com>
2024-02-07 14:03:47 +0100

    test: VI with data_generator

--- tests/vi.py
+++ tests/vi.py
@@ -15,6 +15,48 @@ os.environ['PROGRESSBAR_MINIMUM_UPDATE_INTERVAL'] = "30"
 
 class Test(unittest.TestCase):
 
+    def test_vi_generator(self):
+        """
+        Test MAP estimation with example dataset
+        """
+        with open("tests/data/0.txt") as f:
+            text = f.read().lower().split()
+        text, vocabulary = preprocess_standard(text)
+
+        vocab_size = len(vocabulary)
+        batch_size = 250
+        dim = 25
+        N = len(text)
+        e = Embedding(vocabulary=vocabulary, dimensionality=dim)
+
+        def data_generator():
+            while True:
+                i = np.random.choice(text, batch_size)
+                j = np.random.choice(text, batch_size)
+                x = np.random.binomial(1, 0.25, size=batch_size)
+
+                i = tf.constant(i)
+                j = tf.constant(j)
+                x = tf.constant(x, dtype=tf.float64)
+                yield (i,j,x)
+
+        theta_orig = e.theta.numpy()
+        q_mu, q_std_e, elbo_history = mean_field_vi(e, text, data_generator=data_generator(), N=N, model="sgns", evaluate=False, batch_size=batch_size, init_mean=False, epochs=1, elbo_history=True)
+        theta = q_mu.theta.numpy()
+        self.assertNotEqual(theta_orig[0,0], theta[0,0])
+
+        self.assertEqual(type(theta), np.ndarray)
+
+        theta_shape = theta.shape
+        valid_shape = (vocab_size * 2, dim)
+        self.assertEqual(theta_shape, valid_shape)
+
+        K = 23
+        words = random.choices(text, k=K)
+        embeddings = e[words]
+        self.assertEqual(embeddings.shape, (K, dim))
+
+
     def test_vi(self):
         """
         Test mean-field variational inference with example dataset
